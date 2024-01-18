8ad50c36eeba177f5238036f580e6dc4f819543a
Radek Svoboda <rasvob14@gmail.com>
2024-01-18 15:50:46 +0100

    Home wout video finished

---
+++ src/Components/Icons/SvgIcons.jsx
@@ -0,0 +1,53 @@
+export const WarningIcon = ({isActive}) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`w-6 h-6 ${isActive ? "text-error": ""}`}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+    </svg>
+);
+
+export const HomeIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+    </svg>
+);
+
+export const BoxesPlusIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" />
+    </svg>
+);
+
+export const GlobeIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+    </svg>
+);
+
+export const PlusCircleIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    </svg>
+);
+
+export const MinusCircleIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    </svg>
+);
+
+export const PlusIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+    </svg>
+);
+
+export const RightArrowTop = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+    </svg>
+);
+
+export const CreditCardIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+    </svg>
+);
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { HomeIcon, BoxesPlusIcon,  GlobeIcon, WarningIcon, PlusCircleIcon, PlusIcon, MinusCircleIcon, RightArrowTop, CreditCardIcon } from "../Icons/SvgIcons";
 
 const DatetimeComponent = ({date, time}) => {
     return (
@@ -10,111 +11,7 @@ const DatetimeComponent = ({date, time}) => {
     );
 };
 
-const NowSummaryComponent = ({title, date, time}) => {
-    return (
-        <div className="flex justify-between">
-            <p className="text-4xl">{title}</p>
-            <DatetimeComponent date={date} time={time} />
-        </div>
-    );
-};
-
-const StatItemComponent = ({title, value, desc, descValue=null, icon="home"}) => {
-    const WarningIcon = ({isActive}) => (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`w-6 ${isActive ? "text-error": ""}`}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
-        </svg>
-    );
-
-    const HomeIcon = () => (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-        </svg>
-    );
-
-    const InflowIcon = () => (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" />
-        </svg>
-    );
-
-    const OutflowIcon = () => (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
-        </svg>
-    );
-
-    const iconMap = {
-        "home": <HomeIcon />,
-        "inflow": <InflowIcon />,
-        "outflow": <OutflowIcon />
-    };
-    
-    return (
-        <div className="stat">
-            <div className="stat-figure text-primary">
-            
-            {
-                iconMap[icon]
-            }
-            
-            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-            </svg> */}
-
-            </div>
-            <div className="stat-title text-xl">{title}</div>
-            <div className="stat-value">{value}</div>
-            <div className="stat-desc mt-2 text-xl">
-                <div className="flex-inline flex items-center">
-                    <span className="mr-2"><WarningIcon isActive={descValue > 0} /></span>
-                    <span className="">{desc}{descValue}</span>
-                </div>
-            </div>
-        </div>
-    );
-};
-
-const CurrentStatsComponent = ({data}) => {
-    const arrowIcon = ( 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-2 h-2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-        </svg>
-    );
-    
-    const statsItems = [
-        {title: "Počet lodí v přístavu", value: 20, desc: "Nerozpoznaných: ", descValue: 2, icon:"home"},
-        {title: "Dnes připlutí", value: 14, desc: "Nerozpoznaných: ", descValue: 2, icon:"inflow"},
-        {title: "Dnes odplutí", value: 6, desc: "Nerozpoznaných: ", descValue: 0, icon:"outflow"},
-    ];
-
-    return (
-        <div className="stats shadow">
-
-            {
-                statsItems.map((item, index) => (
-                    <StatItemComponent key={index} title={item.title} value={item.value} desc={item.desc} descValue={item.descValue} icon={item.icon} />
-                ))
-            }
-
-            <div className="stat">
-                <div className="flex flex-col items-center justify-center">
-                    <Link to="/state-details">
-                        <div className="text-secondary btn btn-circle btn-outline btn-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
-                            </svg>
-                        </div>
-                    </Link>
-                </div>
-                {/* <div className="stat-value text-secondary">2.6M</div> */}
-            </div>
-            
-        </div>
-    );
-};
-
-const Home = () => {
+const NowSummaryComponent = ({title}) => {
     const [time, setTime] = useState(new Date().toLocaleTimeString());
     const [date, setDate] = useState(new Date().toLocaleDateString());
 
@@ -126,14 +23,105 @@ const Home = () => {
     }, []);
 
     return (
+        <div className="flex justify-between">
+            <p className="text-4xl">{title}</p>
+            <DatetimeComponent date={date} time={time} />
+        </div>
+    );
+};
+
+const StatLink = ({to, icon}) => {
+    return (
+        <div className="stat">
+            <div className="flex flex-col items-center justify-center">
+                <Link to={to}>
+                    <div className="text-secondary btn btn-circle btn-outline btn-secondary">
+                        {icon}
+                    </div>
+                </Link>
+            </div>
+        </div>
+    )
+};
+
+const StatItemComponent = ({title, value, desc, descValue=null, icon=null, altIcon=null}) => {
+    return (
+        <div className="stat">
+            <div className="stat-figure text-primary">
+                {icon}
+            </div>
+            <div className="stat-title text-xl">{title}</div>
+            <div className="stat-value">{value}</div>
+            <div className="stat-desc text-xl">
+                <div className="flex-inline flex items-center mt-2">
+                    <span className="mr-2">{altIcon}</span>
+                    <span className="">{desc}{descValue}</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const CurrentStatsComponent = () => {
+    const statsItems = [
+        {title: "Počet lodí v přístavu", value: 20, desc: "Nerozpoznaných: ", descValue: 2, icon: "home"},
+        {title: "Dnes připlutí", value: 14, desc: "Nerozpoznaných: ", descValue: 2, icon: "inflow"},
+        {title: "Dnes odplutí", value: 6, desc: "Nerozpoznaných: ", descValue: 0, icon: "outflow"},
+    ];
+
+    const getIconByName = (name) => {
+        switch (name) {
+            case "home":
+                return <HomeIcon />;
+            case "inflow":
+                return <PlusCircleIcon />;
+            case "outflow":
+                return <MinusCircleIcon />;
+            default:
+                return <GlobeIcon />;
+        }
+    };
+
+    return (
+        <div className="shadow stats">
+            {
+                statsItems.map((item, index) => {
+                    return (
+                        <StatItemComponent key={index} title={item.title} value={item.value} desc={item.desc} descValue={item.descValue} icon={getIconByName(item.icon)} altIcon={<WarningIcon isActive={item.descValue > 0} />} />
+                    );
+                })
+            }
+            <StatLink to="/state-details" icon={<RightArrowTop />} />
+        </div>
+    );
+};
+
+const PaymentsStats = () => {
+    const paymentStatsItem =  {'payed': 21, 'unpayed': 4};
+    
+    return (
+        <div className="stats shadow">
+            <StatItemComponent title="Zaplacené lodě" value={paymentStatsItem['payed']} icon={paymentStatsItem['payed'] == 0 ? <WarningIcon isActive={true} /> : null } />
+            <StatItemComponent title="Nezaplacené lodě" value={paymentStatsItem['unpayed']} icon={paymentStatsItem['unpayed'] > 0 ? <WarningIcon isActive={true} /> : null } />
+
+            <StatLink to="/payments" icon={<CreditCardIcon />} />
+        </div>
+    );
+};
+
+const Home = () => {
+    return (
         <div className="container mx-auto">
             <div className="my-2">
-                <NowSummaryComponent title="Aktuální přehled" date={date} time={time} />             
+                <NowSummaryComponent title="Aktuální přehled" />
             </div>
 
-            <div className="my-2">
+            <div className="my-2 flex justify-between flex-wrap space-y-4 2xl:space-y-0">
                 <CurrentStatsComponent data={null} />
+                <PaymentsStats />
             </div>
+
+            <div className="mt-4 skeleton w-full h-60"></div>
         </div>
     );
 };  
